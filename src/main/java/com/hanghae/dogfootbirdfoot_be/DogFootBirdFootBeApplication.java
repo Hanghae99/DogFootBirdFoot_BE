6904485a3cfb0a4c5bdc0949ac2eb401dcde4deb
langho968 <langho968@gmail.com>
2022-04-08 19:22:59 +0900

    base

---
+++ src/main/java/com/hanghae/dogfootbirdfoot_be/DogFootBirdFootBeApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.dogfootbirdfoot_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogFootBirdFootBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogFootBirdFootBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/dogfootbirdfoot_be/DogFootBirdFootBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.dogfootbirdfoot_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogFootBirdFootBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
