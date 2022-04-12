c3dc8bd02992e99c20752b7c1139a83e369e9cd1
kim-ju-ho <kim1jo2ho3@naver.com>
2022-04-12 21:27:47 +0900

    comment

--- src/main/java/com/hanghae/dogfootbirdfoot_be/service/CommentService.java
+++ src/main/java/com/hanghae/dogfootbirdfoot_be/service/CommentService.java
@@ -1,9 +1,7 @@
 package com.hanghae.dogfootbirdfoot_be.service;
 
-import com.hanghae.dogfootbirdfoot_be.model.Comment;
 import com.hanghae.dogfootbirdfoot_be.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +11,10 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
 
-    public Page<Comment> getMyComments(User user) {
-        return commentRepository.findAllByUserOrderByCreatedAtDesc(user, PageRequest.of(0,5));
-    }
+//    public Page<Comment> getMyComments(User user) {
+//        return commentRepository.findAllByUserOrderByCreatedAtDesc(user, PageRequest.of(0,5));
+//    }
 
     //페이징 있어야 되는걸까? 아마도?
+
 }
