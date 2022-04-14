bd5315652fcfb7ddc607adf91efbf603d97d5c5b
kim-ju-ho <kim1jo2ho3@naver.com>
2022-04-14 16:50:09 +0900

    수정

--- src/main/java/com/hanghae/dogfootbirdfoot_be/controller/HomeController.java
+++ src/main/java/com/hanghae/dogfootbirdfoot_be/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.hanghae.dogfootbirdfoot_be.controller;
 
+import com.hanghae.dogfootbirdfoot_be.dto.SignupRequestDto;
+import com.hanghae.dogfootbirdfoot_be.dto.SignupResponseDto;
 import com.hanghae.dogfootbirdfoot_be.security.UserDetailsImpl;
 import com.hanghae.dogfootbirdfoot_be.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,11 +18,18 @@ public class HomeController {
     private final UserService userService;
 
 
-    @PostMapping("/api/home")
-    public String getHome(@AuthenticationPrincipal UserDetailsImpl userDetails){
+    @PostMapping("/api/userChk")
+    public SignupResponseDto getHome(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        HashMap<String,String> hashMap = new HashMap<>();
         if(userDetails!=null){
-        return userDetails.getUsername();
-    }
-        return "로그인 안됨";
+            hashMap.put("result","true");
+            hashMap.put("msg" , "로그인한 사용자입니다.");
+            return new SignupResponseDto(userDetails,hashMap);
+        }else{
+            hashMap.put("result","false");
+            hashMap.put("msg" , "로그인이 필요한 사용자입니다.");
+            return new SignupResponseDto(hashMap);
+        }
+
 }
 }
--- src/main/java/com/hanghae/dogfootbirdfoot_be/dto/SignupResponseDto.java
+++ src/main/java/com/hanghae/dogfootbirdfoot_be/dto/SignupResponseDto.java
@@ -1,24 +1,27 @@
 package com.hanghae.dogfootbirdfoot_be.dto;
 
 import com.hanghae.dogfootbirdfoot_be.security.UserDetailsImpl;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-public class SignupRequestDto {
+import java.util.HashMap;
+
+public class SignupResponseDto {
+    private HashMap<String,String> result;
     private Long userId;
     private String username;
     private String nickname;
-    private String password;
     private String userProfile;
     private String email;
 
-    public SignupRequestDto(UserDetailsImpl userDetails) {
+    public SignupResponseDto(UserDetailsImpl userDetails, HashMap<String,String> result) {
+        this.result=result;
         this.userId = userDetails.getUser().getUserId();
         this.username = userDetails.getUsername();
         this.nickname = userDetails.getUser().getNickname();
         this.userProfile = userDetails.getUser().getUserProfile();
         this.email = userDetails.getUser().getEmail();
+    }
+
+    public SignupResponseDto(HashMap<String,String> result){
+        this.result=result;
     }
 }
