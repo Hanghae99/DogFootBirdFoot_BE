508c77e6957536c0ffba5e56ded623bc7afc2df4
TodayIsYolo <a1991015@gmail.com>
2022-04-14 16:47:33 +0900

    1

--- src/main/java/com/hanghae/dogfootbirdfoot_be/dto/UserImageRequestDto.java
+++ src/main/java/com/hanghae/dogfootbirdfoot_be/dto/UserImageRequestDto.java
@@ -3,6 +3,7 @@ package com.hanghae.dogfootbirdfoot_be.dto;
 import lombok.Getter;
 
 @Getter
+//수정
 public class UserImageRequestDto {
     private String imageUrl;
     public UserImageRequestDto(String uploadImageUrl){
