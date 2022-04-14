84d02761f354da18fbd441ddbccc3f9ad9958fdf
langho968 <langho968@gmail.com>
2022-04-14 16:16:49 +0900

    유저 이미지 업데이트 추가

--- src/main/java/com/hanghae/dogfootbirdfoot_be/controller/FileUploadController.java
+++ src/main/java/com/hanghae/dogfootbirdfoot_be/controller/FileUploadController.java
@@ -3,8 +3,11 @@ package com.hanghae.dogfootbirdfoot_be.controller;
 
 import com.hanghae.dogfootbirdfoot_be.dto.PostImageRequestDto;
 import com.hanghae.dogfootbirdfoot_be.dto.UserImageRequestDto;
+import com.hanghae.dogfootbirdfoot_be.security.UserDetailsImpl;
 import com.hanghae.dogfootbirdfoot_be.service.S3Uploader;
+import com.hanghae.dogfootbirdfoot_be.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -20,7 +23,7 @@ import java.io.IOException;
 public class FileUploadController {
 
     private final S3Uploader s3Uploader;
-
+    private final UserService userService;
     // 이미지 가져오기
     @PostMapping("/api/post/images")
     private PostImageRequestDto uploadPostImage(@RequestPart("images") MultipartFile multipartFile) throws IOException {
@@ -34,12 +37,13 @@ public class FileUploadController {
     }
 
     @PostMapping("/api/mypage/userProfile")
-    private UserImageRequestDto uploadUserImage(@RequestPart("images") MultipartFile multipartFile) throws IOException {
+    private UserImageRequestDto uploadUserImage(@RequestPart("images") MultipartFile multipartFile, @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
         System.out.println(multipartFile.getOriginalFilename());
         System.out.println(multipartFile.getContentType());
         System.out.println(multipartFile.getSize());
         System.out.println(multipartFile.getName());
         UserImageRequestDto userImageRequestDto = new UserImageRequestDto(s3Uploader.upload(multipartFile, "static"));
+        userService.uploadUserImage(userImageRequestDto, userDetails);
         return userImageRequestDto;
     }
 }
